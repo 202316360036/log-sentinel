173bdd9a473f353c6ba931a988a33907a0c6227d
Helena Santos Freitas <202316360007@ifba.edu.br>
2026-08-09 19:58:21 -0300

    Implement test for MainWindow opening
    Add a test for the MainWindow title.

--- tests/tests/gui/test_main_window.py
+++ tests/tests/gui/test_main_window.py
@@ -1,1 +1,11 @@
+```python
+from python_pdm_template.gui.main_window import MainWindow
+
+
+def test_janela_abre(qtbot):
+    w = MainWindow()
+    qtbot.addWidget(w)
+
+    assert w.windowTitle() == "Log Sentinel"
+```
 
